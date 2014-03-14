e14955d2b40f6dd1213edd98bc9faca18e32c487
Eduardo Sorribas <eduardo@sorribas.org>
2014-03-14 11:18:27 +0100

    Make callback optional

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var util = require('util');
 
 module.exports = function(arr, iterator, cb) {
 	var i = 0;
+	if (!cb) cb = function() {};
  
 	var loop = function() {
 		if (i >= arr.length) return cb();
